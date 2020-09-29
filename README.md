9a6ff672a803981a54a93df7d0bd74c3b30c8c07
Blackjack2020 <69491789+devonp702@users.noreply.github.com>
2020-09-28 18:43:02 -0600

    event listeners, and text display added

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-make each question an object in an array called quiz
-click start button
-timer starts
-function for handling objects and displaying data on screen
-function for keeping score.
-end game and save score.
+# jsQuiz
+[Lives Here](https://github.com/devonp702/jsQuiz)
+
+## Page Introduction
+This is a quiz to test your knowledge of Java Script. It will give you two minutes to answer as many questions as possible. A correct answer will give you a point, an incorrect answer will give you a time penalty. Leave your name and high-score at the end.
+
+## Goals
+* make each question an object in an array
+* click start button to start game
+* timer counts down
+* function for handling objects and displaying data on screen
+* function for keeping score.
+* end game and save score.
--- script.js
+++ script.js
@@ -5,35 +5,38 @@
 //main code body here
 
 // click the start button > add event listener, hide button, start program.
-var startBtn = document.querySelector("#start");
+var startBtn = document.querySelector("#start"); //get the start button
 
-startBtn.addEventListener("click", startQuiz);
+startBtn.addEventListener("click", startQuiz); // when click start button do startQuiz function
 
-function startQuiz() {
-  startBtn.style.display = "none";
-  setTimer();
-  console.log("started.");
+function startQuiz() { //things that happen when start button is clicked
+  startBtn.style.display = "none"; // hide the now useless button
+  setTimer(); // start the count down
+  // anything that starts when button is clicked
 }
 // library of code question objects
-var quizLib = [{
+var quizLib = [{ // access a question with quizLib[number]
     question: "What is the answer?",
     op1: "A",
     op2: "B",
     op3: "C",
-    op4: "D"
+    op4: "D",
+    c: "4"
   },
   {
     question: "What is the new answer?",
     op1: "1",
     op2: "2",
     op3: "3",
-    op4: "4"
+    op4: "4",
+    c: "6"
   }
 ];
-console.log(quizLib[0])
+
 // countdown timer
 var timeLeft = 120;
-var timeElement = document.getElementById("time")
+var timeElement = document.getElementById("time");
+
 function setTimer(){
   var timer = setInterval(function() {
     timeLeft--;
@@ -42,13 +45,30 @@ function setTimer(){
     if(timeLeft === 0) {
       clearInterval(timer);
       alert("time is up");
-      // end screen function here
+      // game over screen function here
     }
   } ,1000);
 }
 
 // function to display question
+var display = document.getElementById("display");
 
+function displayText(num) {
+  var show = quizLib[num];
+  display.innerHTML = "<h2></h2><br><button></button><br><button></button><br><button></button><br><button></button>";
+  display.childNodes[0].textContent = show.question;
+  display.childNodes[2].textContent = show.op1;
+  display.childNodes[4].textContent = show.op2;
+  display.childNodes[6].textContent = show.op3;
+  display.childNodes[8].textContent = show.op4;
+  display.children[show.c].setAttribute("data-correct", "true");
+}
+displayText(0);
 // WHEN I answer a question THEN I am presented with another question >loop and function
-// WHEN I answer a question incorrectly THEN time is subtracted from the clock > function if (correct) add points if (incorrect) minus time
+display.addEventListener("click", function(e) {
+  e.preventDefault;
+  console.log(e.target);
+});
+
+// WHEN I answer a question incorrectly THEN time is subtracted from the clock > function if (correct) add point if (incorrect) minus time
 // WHEN all questions are answered or the timer reaches 0 THEN the game is over AND I can save my initials and score combine into function
