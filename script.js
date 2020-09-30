e062d01c6d5a5e6a2f361264b363a9a9924fa737
Blackjack2020 <69491789+devonp702@users.noreply.github.com>
2020-09-29 18:11:16 -0600

    new functions and variables.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //global variables go here
-
+var points = 0;
 //functions declared here
 
 //main code body here
@@ -8,14 +8,16 @@
 var startBtn = document.querySelector("#start"); //get the start button
 
 startBtn.addEventListener("click", startQuiz); // when click start button do startQuiz function
-
+var play = false;
 function startQuiz() { //things that happen when start button is clicked
   startBtn.style.display = "none"; // hide the now useless button
+  play = true;
   setTimer(); // start the count down
+  getQuestions();
   // anything that starts when button is clicked
 }
 // library of code question objects
-var quizLib = [{ // access a question with quizLib[number]
+var quizLib = [{ // access a question with quizLib[number], op is option and will be a button, c is the correct answer's child address 
     question: "What is the answer?",
     op1: "A",
     op2: "B",
@@ -37,20 +39,20 @@ var quizLib = [{ // access a question with quizLib[number]
 var timeLeft = 120;
 var timeElement = document.getElementById("time");
 
-function setTimer(){
-  var timer = setInterval(function() {
+function setTimer() {
+  var timer = setInterval(function () {
     timeLeft--;
-    timeElement.textContent= "Seconds Left: " + timeLeft;
+    timeElement.textContent = "Seconds Left: " + timeLeft;
 
-    if(timeLeft === 0) {
+    if (timeLeft === 0) {
       clearInterval(timer);
       alert("time is up");
-      // game over screen function here
+      gameOver();
     }
-  } ,1000);
+  }, 1000);
 }
 
-// function to display question
+// function to display question and content
 var display = document.getElementById("display");
 
 function displayText(num) {
@@ -63,12 +65,28 @@ function displayText(num) {
   display.childNodes[8].textContent = show.op4;
   display.children[show.c].setAttribute("data-correct", "true");
 }
-displayText(0);
-// WHEN I answer a question THEN I am presented with another question >loop and function
-display.addEventListener("click", function(e) {
-  e.preventDefault;
-  console.log(e.target);
-});
 
+// WHEN I answer a question THEN I am presented with another question >loop and function
+var getClick = display.addEventListener("click", function (e) {
+  e.preventDefault;
+  return e.target;
+});
+function getQuestions(){
+  for (i = 0; i<quizLib.length; i++) {
+    var load = displayText(i);
+    var click = getClick;
+    console.log(click);
+    while (play) {
+    load
+    if (click.hasAttribute(data-correct)) {
+      points++;
+    }
+    }
+  }
+  gameOver();
+}
 // WHEN I answer a question incorrectly THEN time is subtracted from the clock > function if (correct) add point if (incorrect) minus time
 // WHEN all questions are answered or the timer reaches 0 THEN the game is over AND I can save my initials and score combine into function
+function gameOver(n) {
+  display.textContent = "Game Over";
+}
